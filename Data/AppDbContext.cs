a240209fb9e6fbdabc0014b630f14cf552fe4f5f
nguyenduytuong04 <tuongnguyenduytuong076@gmail.com>
2025-03-28 11:25:00 +0700

    buoi 02

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using NguyenDuyTuong_2122110008_ASP.Model;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace NguyenDuyTuong_2122110008_ASP.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace NguyenDuyTuong_2122110008_ASP.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+    }
+}
